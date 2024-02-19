559faade09d65301dc3a0a326e4012794dde8134
evelynvaida <113584134+evelynvaida@users.noreply.github.com>
2024-02-19 20:11:36 +0200

    API endpoint try

--- src/express.ts
+++ src/express.ts
@@ -8,6 +8,14 @@ server.use(cors())
 
 server.use(express.json())
 
+const QueryParamPriceSchema = z.object({
+    min: z.coerce.number(),
+    max: z.coerce.number()
+})
+/* const QueryParamTypeSchema = z.object({
+    type: z.string(),
+}) */
+
 const ProductSchema = z.object({
     id: z.number(),
     type: z.string(),
@@ -16,7 +24,6 @@ const ProductSchema = z.object({
 })
 
 type Product = z.infer<typeof ProductSchema>
-
 
 const readfile = async () => {
     try {
@@ -29,6 +36,45 @@ const readfile = async () => {
     }
 }
 
-readfile()
+server.get("/api/products", async (req, res) => {
+    const products = await readfile()
+    res.json(products)
+}
+)
+
+server.get("/api/products/price", async (req, res) => {
+    const result = QueryParamPriceSchema.safeParse(req.query)
+    if (!result.success)
+        return res.status(400).json(result.error.issues)
+
+    const products = await readfile()
+    const queryParams = result.data
+    const filteredProducts = products?.filter(product => product.price > queryParams.min && product.price < queryParams.max)
+    res.json(filteredProducts)
+}
+)
+
+server.get("/api/products/:type", async (req, res) => {
+    const type = req.params.type
+    const products = await readfile()
+    if (!products)
+        return res.sendStatus(500)
+
+    const filteredProductTypes = products?.filter(product => product.type === type)
+    res.json(filteredProductTypes)
+
+     /* const result = QueryParamPriceSchema.safeParse(req.query)
+    if (!result.success)
+        return res.status(400).json(result.error.issues)
+    const queryParams = result.data
+    const filteredProductPrice = filteredProductTypes.filter(product => product.price > queryParams.min && product.price < queryParams.max)
+    if (result) {
+        res.json(filteredProductPrice)
+    } else {
+        res.json(filteredProductTypes)
+    }
+    // res.json(fileteredProductPrice) */
+} 
+)
 
 server.listen(4000)
